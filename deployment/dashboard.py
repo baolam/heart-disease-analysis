3818c4a41a51dec5210476292108b0079d952f26
baolam <lam01662052827@gmail.com>
2026-03-22 16:07:11 +0700

    Updating dashboard

--- deployment/dashboard.py
+++ deployment/dashboard.py
@@ -2,6 +2,8 @@ import streamlit as st
 import sqlite3
 import pandas as pd
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import seaborn as sns
